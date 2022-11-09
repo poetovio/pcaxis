0056d6317d049a32ace7be855115da49a8eb40a6
poetovio <urban.vizintin@outlook.com>
2022-11-09 23:59:14 +0100

    Koncano, potrebno dodati nemutave podatke

--- peesieksis.js
+++ peesieksis.js
@@ -2,6 +2,7 @@ let https = require('https');
 let px = require('px');
 let fs = require('fs');
 
+/*
 https.get('https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701084S.PX', (res,err,data) => {
     res.on('data', data => {
         
@@ -12,12 +13,13 @@ https.get('https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701084S
         let zacetek = peiks.dataDict([0, '*']);
         
         try {
-            fs.writeFileSync('./zacetek.json', JSON.stringify(zacetek))
+            fs.writeFileSync('./zacetek.json', JSON.stringify(peiks.dataDict([0, '*'])))
         } catch(err) {
             console.err(err);
         }
     });
 });
+*/
 
 https.get('https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701099S.PX', (res,err,data) => {
     res.on('data', data => {
@@ -25,10 +27,8 @@ https.get('https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701099S
         peiks.variables();
         peiks.values('MESEC');
 
-        let konec = peiks.dataDict([0, '*']);
-
         try {
-            fs.writeFileSync('./danes.json', JSON.stringify(konec))
+            fs.writeFileSync('./danes.json', JSON.stringify(peiks.dataDict([0, '*'])))
         } catch(err) {
             console.err(err);
         }
