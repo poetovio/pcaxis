170c0202d77469e6510186fa303891a2e2833bc7
poetovio <urban.vizintin@outlook.com>
2022-11-10 00:13:28 +0100

    Poskus popravka - failed

--- peesieksis.js
+++ peesieksis.js
@@ -2,24 +2,22 @@ let https = require('https');
 let px = require('px');
 let fs = require('fs');
 
-/*
-https.get('https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701084S.PX', (res,err,data) => {
+
+https.get('	https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701084S.PX', (res,err,data) => {
     res.on('data', data => {
         
         peiks = new px(data.toString());
         peiks.variables();
         peiks.values('MESEC');
         
-        let zacetek = peiks.dataDict([0, '*']);
-        
         try {
-            fs.writeFileSync('./zacetek.json', JSON.stringify(peiks.dataDict([0, '*'])))
+            fs.writeFileSync('./vcasih.json', JSON.stringify(peiks.dataDict([0, '*'])))
         } catch(err) {
             console.err(err);
         }
     });
 });
-*/
+
 
 https.get('https://pxweb.stat.si/SiStatData/Resources/PX/Databases/Data/0701099S.PX', (res,err,data) => {
     res.on('data', data => {
